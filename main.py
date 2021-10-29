dcdd1fcea087a0b9cbf36b2bd11a340f5c78c91b
Lukas Rambold <lukas@rambold.io>
2021-10-30 01:20:22 +0200

    return spanning tree node order in bfs

--- main.py
+++ main.py
@@ -6,44 +6,7 @@ import numpy as np
 from math import sqrt
 from queue import SimpleQueue
 
-mesh = om.PolyMesh()
-
-# add a a couple of vertices to the mesh
-vh0 = mesh.add_vertex([0, 1, 1])
-vh1 = mesh.add_vertex([1, 0, 1])
-vh2 = mesh.add_vertex([2, 1, 1])
-vh3 = mesh.add_vertex([0,-1, 1])
-vh4 = mesh.add_vertex([2,-1, 1])
-
-# add a couple of faces to the mesh
-fh0 = mesh.add_face(vh0, vh1, vh2)
-fh1 = mesh.add_face(vh1, vh3, vh4)
-fh2 = mesh.add_face(vh0, vh3, vh1)
-
-# add another face to the mesh, this time using a list
-vh_list = [vh2, vh1, vh4]
-fh3 = mesh.add_face(vh_list)
-
-#  0 ==== 2
-#  |\  0 /|
-#  | \  / |
-#  |2  1 3|
-#  | /  \ |
-#  |/  1 \|
-#  3 ==== 4
-
-# get the point with vertex handle vh0
-point = mesh.point(vh0)
-
-# get all points of the mesh
-point_array = mesh.points()
-
-# translate the mesh along the x-axis
-point_array += np.array([1, 0, 0])
-
-# write and read meshes
-om.write_mesh('test.stl', mesh)
-mesh = om.read_trimesh('test.stl')
+mesh = om.read_trimesh('stanford_bunny.stl')
 mesh.update_normals()
 
 # making it easier to read the OpenMesh API
@@ -54,17 +17,21 @@ get_vertecies_by_face = lambda mesh, face: mesh.fv(face)
 def bfs(mesh, start_face):
 	visited_faces = set()
 	queue = SimpleQueue()
+	tour = []
 	
 	queue.put(start_face)
 	visited_faces.add(start_face.idx())
 
 	while not queue.empty():
 		current_face = queue.get()
+		tour.append(current_face.idx())
 
 		for adjacent_face in get_adjacent_faces(mesh, current_face):
 			if adjacent_face.idx() not in visited_faces:
 				visited_faces.add(adjacent_face.idx())
 				queue.put(adjacent_face)
+	
+	return tour
 
 		print(current_face.idx())
 		# print(visited_faces)
