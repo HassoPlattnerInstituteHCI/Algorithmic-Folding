cc17651def05e791916a509ad4c5e5f6ce254e8b
Lukas Rambold <lukas@rambold.io>
2021-10-31 22:25:54 +0100

    yay works

--- main.py
+++ main.py
@@ -3,17 +3,23 @@ import openmesh as om
 # https://openmesh-python.readthedocs.io/en/latest/iterators.html
 
 import numpy as np
+from numpy.linalg import inv
+
 from math import sqrt, pi
 from queue import SimpleQueue
 from collections import defaultdict
 
-mesh = om.read_trimesh('cube.ply')
+import drawSvg as draw
+
+mesh = om.read_trimesh('stanford_bunny.stl')
 mesh.update_normals()
 
 # making it easier to read the OpenMesh API
 get_adjacent_faces = lambda mesh, face: mesh.ff(face)
 get_vertecies_by_face = lambda mesh, face: mesh.fv(face)
 
+
+d = draw.Drawing(10, 10, origin='center', displayInline=False)
 
 def bfs(mesh, start_face):
 	visited_faces = set()
@@ -35,29 +41,12 @@ def bfs(mesh, start_face):
 				queue.put(adjacent_face)
 				visited_faces.add(adjacent_face.idx())
 				insert_to_spanningtree(node_id=adjacent_face.idx(), parent_id=current_face.idx())
-				
+
 	return spanning_tree
 
 def is_polygon_overlapping(polygon_a, polygon_b):
 	from shapely.geometry import Polygon
 	return Polygon(polygon_a).intersects(Polygon(polygon_b))
-
-def rotation_matrix(axis, theta):
-    """
-    Return the rotation matrix associated with counterclockwise rotation about
-    the given axis by theta radians.
-    source: https://stackoverflow.com/questions/6802577/rotation-of-3d-vector
-    """
-    axis = np.asarray(axis)
-    axis = axis / math.sqrt(np.dot(axis, axis))
-    a = math.cos(theta / 2.0)
-    b, c, d = -axis * math.sin(theta / 2.0)
-    aa, bb, cc, dd = a * a, b * b, c * c, d * d
-    bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
-    return np.array([[aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
-                     [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
-                     [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc]])
-
 
 def get_2d_projection(mesh, face):
 	"""
@@ -67,7 +56,7 @@ def get_2d_projection(mesh, face):
 	nx, ny, nz = mesh.normal(face)
 
 	if nx == 0.0 and ny == 0.0:
-		# nothing to do: points are already parallel to xy-plane
+		# nothing to do: points are already parallel to xy-plane: matrix just gets rid of z-values
 		return np.array([
 			[1,0,0],
 			[0,1,0]
@@ -78,9 +67,6 @@ def get_2d_projection(mesh, face):
 			[nx * ny / sqrt(nx**2+ny**2), ny * nz / sqrt(nx**2 + ny**2), -sqrt(nx**2 + ny**2)]
 		])
 
-def get_world_to_face_plane_mapping_fn(mesh, face):
-	return lambda point: get_2d_projection(mesh, face).dot(point)
-
 def get_edge_between_faces(mesh, face_a, face_b):
 	for face_a_halfedge in mesh.fh(face_a):
 		if mesh.face_handle(mesh.opposite_halfedge_handle(face_a_halfedge)).idx() == face_b.idx():
@@ -88,45 +74,47 @@ def get_edge_between_faces(mesh, face_a, face_b):
 	raise Error("there is no edge between these two faces")
 
 def get_rotation_matrix(axis, angle):
+	# TODO make this work with off-center axis
 	from scipy.spatial.transform import Rotation
-	return Rotation.from_rotvec(angle * axis).as_matrix()
+	return Rotation.from_rotvec(axis/np.linalg.norm(axis) * angle).as_matrix()
+
 
 def unfold(mesh, spanning_tree):
 	root = spanning_tree[-1][0]
 	polygons = []
 
-	def get_relative_rotation(node, parent):
-		node_face = mesh.face_handle(node)
-		parent_face = mesh.face_handle(parent)
-		node_normal = mesh.normal(node_face)
-		parent_normal = mesh.normal(parent_face)
+	def get_mapping(node, parent):
+		crease_halfedge = get_edge_between_faces(mesh,  mesh.face_handle(node),  mesh.face_handle(parent))
+		crease_angle = mesh.calc_dihedral_angle(crease_halfedge)
+		crease_vector = mesh.calc_edge_vector(crease_halfedge)
+		
+		# offset is introduces, since rotation is not occuring around the origin but along crease line
+		offset = mesh.point(mesh.from_vertex_handle(crease_halfedge))
 
-		angle_between_faces = np.arccos(node_normal.dot(parent_normal)) + pi/2
-		rotation_axis = mesh.normal(get_edge_between_faces(mesh, node_face, parent_face))
-		# print(rotation_axis)
-		return get_rotation_matrix(angle_between_faces, rotation_axis)
+		return lambda points: get_rotation_matrix(crease_vector, crease_angle).dot(points - offset) + offset
 
-	def unfolder_recursive_call(node, transform_matrix_heritage):
+	def unfolder_recursive_call(node, mapping_fn):
 		polygon_3d = [mesh.point(vertex_handle) for vertex_handle in get_vertecies_by_face(mesh, mesh.face_handle(node))]
-		polygons.append([transform_matrix_heritage.dot(point_3d) for point_3d in polygon_3d])
+		# apply previous coordinate mapping function and store polygon
+		polygons.append([mapping_fn(point_3d) for point_3d in polygon_3d])
 
 		for child in spanning_tree[node]:
-			# TODO check order of dot op
-			new_transformation = transform_matrix_heritage.dot(get_relative_rotation(node, child))
-			unfolder_recursive_call(child, new_transformation)
+			# add a new mapping function to the 'stack' that maps the child's coordinate system to the parent's
+			# coordinate system. (which in turn will be handed of to the rest of the function stack to end up with the final coordinates)
+			new_mapping = lambda points: mapping_fn(get_mapping(child, node)(points))
+			unfolder_recursive_call(child, new_mapping)
 
-	unfolder_recursive_call(root, get_2d_projection(mesh, mesh.face_handle(root)))
+	map_to_2d = lambda points: get_2d_projection(mesh, mesh.face_handle(root)).dot(points)
+	unfolder_recursive_call(root, map_to_2d)
 
 	return polygons
 
 
 spanning_tree = bfs(mesh, mesh.face_handle(0))
-import drawSvg as draw
 print(unfold(mesh, spanning_tree))
 
-d = draw.Drawing(200, 100, origin='center', displayInline=False)
-
 for polygon in unfold(mesh, spanning_tree):
+	# polygon = [coords[0:2] for coords in polygon]
 	d.append(draw.Lines(*np.array(polygon).flatten(), closed=True,
 	            fill='#eeee00'))
 
