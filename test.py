d9c4da1e0eea9ec5bb9eb4a1c4199689569df685
= <=>
2022-11-29 13:23:53 +0100

    better adjacency

--- test.py
+++ test.py
@@ -21,10 +21,12 @@ class Mesh:
         adj = []
         faces = [[]]
         adjacend_faces, _ = igl.triangle_triangle_adjacency(self.igl_f)
+        face_mapping = {}
 
         normal_vec_equal = lambda a, b: np.array_equal(a, b) or np.array_equal(a * -1, b)
 
         def help(curr_igl_face, faces_index, normal_vec):
+            face_mapping[curr_igl_face] = faces_index
 
             if faces_index >= len(normals_polyhedral):
                 normals_polyhedral.append(normal_vec)
@@ -43,15 +45,20 @@ class Mesh:
                     continue
                 if not normal_vec_equal(normals[f], normals[curr_igl_face]):
                     faces.append([])
-                    adj.append((faces_index, len(faces) - 1))
                     help(f, len(faces) - 1, normals[f])
 
         help(0, 0, normals[0])
+        for igl_f, af in enumerate(adjacend_faces):
+            for f in af:
+                adj.append((face_mapping[igl_f], face_mapping[f]))
+
+        # TODO: remove duplicates in adj
+            
         return faces, adj, normals_polyhedral
 
     def __init__(self, path):
         self.igl_v, self.igl_f = igl.read_triangle_mesh("./cube.stl")
-        self.igl_v, self.igl_f, h = igl.remove_duplicates(self.igl_v, self.igl_f, 0.00001)
+        self.igl_v, self.igl_f, _ = igl.remove_duplicates(self.igl_v, self.igl_f, 0.00001)
         self.v = self.igl_v
         self.faces, self.adjacend_faces, self.normals = self.__build_polyhedral_mesh()
 
